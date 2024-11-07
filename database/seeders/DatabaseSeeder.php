1fa89e8e4924ed5991f1e062be5f397ec9cfa247
Mohmmadshabrani <m7madsh206@gmail.com>
2024-11-07 09:57:44 +0100

    hot-fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\Question;
 use Illuminate\Database\Seeder;
+use Database\Seeders\RolesSeeder;
 use Database\Seeders\UsersSeeder;
+use Database\Seeders\ProfilesSeeder;
+use Database\Seeders\CompetenciesSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,7 +19,7 @@ class DatabaseSeeder extends Seeder
         // Question::factory(10)->create();
         
         $this->call([
-            rolesSeeder::class,
+            RolesSeeder::class,
             UsersSeeder::class,
             ProfilesSeeder::class,
             CompetenciesSeeder::class,
