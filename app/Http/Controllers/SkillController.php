a0bea7a390d664eddc61e33a18a002721863d711
Mohmmadshabrani <m7madsh206@gmail.com>
2024-11-27 09:46:41 +0100

    fixed

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -16,7 +16,7 @@ class SkillController extends Controller
         $relations = $request->query('with') ? explode(',', $request->query('with')) : [];
         $relations[] = 'competency';
         $skills = Skill::with($relations)->filter($request)->paginate($request->query('per_page', 10));
-        $competencies = $request->user()->competencies();
+        $competencies = $skills->pluck('competency')->unique('id');
         $resoruce = $this->getResource();
 
         return $resoruce::collection($skills)->additional(['meta' => ['competencies' => $competencies]]);
