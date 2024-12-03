b7ed489498a07ba7e5c98266739f2b5c88f710af
Mohmmadshabrani <m7madsh206@gmail.com>
2024-12-03 11:53:59 +0100

    remove-dd

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,6 @@ class UserController extends Controller
     public function getRoles()
     {
         $roles = Role::all();
-        dd($roles->toArray());
         return response()->json($roles);
     }
 
